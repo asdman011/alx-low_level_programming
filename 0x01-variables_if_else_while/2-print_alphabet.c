36eeead3a4309db3b9695d140a8b8f6a17aa7102
Ammar Ashour <am9370337@gmail.com>
2023-08-11 15:20:07 -0700

    New File

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  char ch = 'a';
+  while (ch <= 'z')
+    {
+      putchar(ch);
+	ch++;
+    }
+  return (0);
+}
