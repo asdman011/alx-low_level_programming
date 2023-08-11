3c7f00d1bd11f95e23a130f68cc717fa49458e15
Ammar Ashour <am9370337@gmail.com>
2023-08-11 15:45:13 -0700

    New commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  char ch = 'a';
+  while (ch <= 'z' && ch != 'q', 'e')
+    {
+      putchar(ch);
+        ch++;
+    }
+  putchar('\n');
+  return (0);
+}
