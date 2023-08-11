97fd01dd4ff205d04499c21d27bd2a664e4d22f2
Ammar Ashour <am9370337@gmail.com>
2023-08-11 16:06:07 -0700

    New commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  char ch = '0';
+  while (ch <= 'F')
+    {
+      putchar(ch);
+        ch++;
+    }
+  putchar('\n');
+  return (0);
+}
