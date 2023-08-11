80f3344fe1fdec0ac5d2e990829fd74a5367ed84
Ammar Ashour <120216095+asdman011@users.noreply.github.com>
2023-08-12 02:43:43 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-  int digit = 48;
-  
-  while (digit <= 102)
+    int digit = 48;
+
+    while (digit <= 102)
     {
-      putchar(digit);
-      if (digit == 57)
-	digit += 39;
-      digit++;
+        putchar(digit);
+        if (digit == 57)
+            digit += 39;
+        digit++;
     }
-  putchar('\n');
-  return (0);
+    putchar('\n');
+    return (0);
 }
